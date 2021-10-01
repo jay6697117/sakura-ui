5197664a03427aa5c9045c63c011fb734b948a5b
steve zhang <871506263@qq.com>
2021-10-01 17:52:36 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Demo from './demo.vue'
+import VueHighlightJS from 'vue-highlightjs'
+
+// Tell Vue.js to use vue-highlightjs
+Vue.use(VueHighlightJS)
+
+Vue.config.productionTip = false
+const context = require.context('./', true, /^((?!demo\.vue).)+\.vue$/)
+console.log(`context.keys():`, context.keys())
+try {
+  context.keys().forEach(path => {
+    let res = context(path)
+    console.log(`res:`, res)
+    Vue.component(res.default.name, res.default)
+  })
+} catch (e) {
+  console.log(e)
+}
+
+new Vue({
+  render: h => h(Demo)
+}).$mount('#app')
